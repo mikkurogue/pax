7f8e5dc13257f96755c06a2df6eda1c63a75736b
mikkurogue <michael.lindemans@outlook.com>
2025-01-08 12:20:47 +0100

    add brainfart scaffold boilerplate

--- src/cli/init_prj.zig
+++ src/cli/init_prj.zig
@@ -2,3 +2,7 @@ const std = @import("std");
 
 /// Initialise a new zig project in the current folder.
 pub fn init_project() !void {}
+
+/// Scaffold the project - essentially make sure we have the project config file
+/// too for the dependencies ready
+fn scaffold() !void {}
