eb842eea943900df288a3b9872a44057ed7126ad
mikkurogue <michael.lindemans@outlook.com>
2025-01-08 13:11:29 +0100

    comment out some tests, i want to get the first test working first
    i believe i may be missing some logic here
    maybe worth splitting the create config function into multiple functions
    to also remove multiple allocs
    also, arena allocater is prob best here? it all needs to be freed at the
    ssame time anyway so like why not?

--- src/lib/config.test.zig
+++ src/lib/config.test.zig
@@ -2,25 +2,31 @@ const std = @import("std");
 const config = @import("config.zig");
 const fs = std.fs;
 
+// TODO:
+// make these tests pass, for now for instance the first test fails
+// because the path can not be found or created or something
+// maybe im brainless maybe im stupid for not using a library to handle this
+// ironic, a package manager who wants to use no packages...
+
 test "create_initial_config creates default config file" {
     try config.create_initial_config();
 }
 
-test "write_to_config handles install action" {
-
-    // Ensure initial config exists
-    try config.create_initial_config();
-
-    try config.write_to_config("example-package", "install");
-}
-
-test "read_from_config verifies installed package" {
-
-    // Ensure initial config and package installation
-    try config.create_initial_config();
-    try config.write_to_config("example-package", "install");
-
-    try config.read_from_config("example-package");
-
-    // If no error is thrown, the package exists, and the test passes
-}
+// test "write_to_config handles install action" {
+//
+//     // Ensure initial config exists
+//     try config.create_initial_config();
+//
+//     try config.write_to_config("example-package", "install");
+// }
+//
+// test "read_from_config verifies installed package" {
+//
+//     // Ensure initial config and package installation
+//     try config.create_initial_config();
+//     try config.write_to_config("example-package", "install");
+//
+//     try config.read_from_config("example-package");
+//
+//     // If no error is thrown, the package exists, and the test passes
+// }
